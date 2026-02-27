09a3fadb198aab46fea2492c3244d604f1825cb8
Anton Yakovenko <anya@itu.dk>
2026-02-27 16:03:10 +0100

    fixed new ip address

--- repositories.py
+++ repositories.py
@@ -87,8 +87,8 @@ GROUP_REPOS = [
         "group p",
         "EastTops",
         ["https://github.com/AntohaY/itu-minitwit"],
-        "http://167.172.106.1",
-        "http://167.172.106.1",
+        "http://164.90.229.101",
+        "http://164.90.229.101",
     ],
     [
         "group q",
